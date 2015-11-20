820698c94390197e5f321a1f399e4255d9eac273
katsiam <katsia.m@live.com>
2015-11-19 21:34:21 -0500

    How to Parse HTMLs
    Added code to parse Cooking html for cook time, special diet tags,
    ratings, rating counts, ingredients

--- B_send_htmlreqs.py
+++ B_send_htmlreqs.py
@@ -6,10 +6,11 @@ Author: Katsia M
 Date: 11/10/2015
 '''
 
-import pandas as pd
+#import pandas as pd
 import json
 import requests
 from bs4 import BeautifulSoup
+import re
 
 
 #TODO: loop through multiple files
@@ -42,12 +43,52 @@ else:
  print('End of script')
  
 web_url_uniq1=list(set(web_url_uniq))
-###
+
+#create a list of dictionaries by url
+startdata=[]
 
 #create and parse html requests
-#TODO: loop through a list of links
-links='http://cooking.nytimes.com/recipes/1015947-roasted-carrots-with-turmeric-and-cumin'
-r = requests.get(links)
-soup = BeautifulSoup(r.text, 'html.parser')
+for links in web_url_uniq1:
+ r = requests.get(links)
+ soup = BeautifulSoup(r.text, 'html.parser')
+ newdic={}
+ #store url, cooktime, special diets tags, ratingvalue, ratingCount, ingredients
+ newdic['url']=links
+
+ if soup.header.div.ul.li.meta['itemprop']=='cookTime':
+  newdic['cookTime']=soup.header.div.ul.li.get_text()
+ else:
+  newdic['cookTime']="None"
+  
+ specdiet=[]
+ zz=soup.find_all("p", 'special-diets tag-block')
+ for i in range(1,len(str(zz).split('special-diet\">'))):
+  a_=re.search('</a>', str(zz).split('special-diet\">')[i]).start()
+  b_=str(zz).split('special-diet\">')[i][0:a_]
+  print(b_)
+  specdiet.append(b_)
+ else:
+  specdiet.append("None")
+
+ newdic['specialDiet']=specdiet
+ 
+ newdic['ratingVlue']=soup.find(itemprop="ratingValue").get_text()
+ newdic['ratingCount']=soup.find_all(itemprop="ratingCount").get_text()
+ 
+ ingredients=[]
+ zz1 = soup.find_all("span", attrs={"itemprop": "name"})
+ for i in range(1,len(zz1)):
+  c_=zz1[i].get_text()
+  if c_ != "The New York Times Company":
+   ingredients.append(c_)
+
+ newdic['recipeIngredient']=ingredients
+
+startdata.append(newdic)
+
+with open('links'+infile, "w") as outfile:
+  json.dump(startdata, outfile, indent=4)
+  
+print('Job #2 is done, madam') 
 
 
